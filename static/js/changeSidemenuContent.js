54e7b6ef96b65cfff61b8fc232328f55f8d4fac8
Schmidbauer Florian <florian.schmidbauer@htlwy.at>
2025-11-11 11:57:00 +0100

    rules fade

--- static/js/changeSidemenuContent.js
+++ static/js/changeSidemenuContent.js
@@ -1,4 +1,11 @@
 function getRules(){
-    document.getElementById("sm-rules").style.display="block";
-    document.getElementById("sm-player").style.display="none";
+    if (document.getElementById("sm-rules").style.display==="block"){
+        document.getElementById("sm-rules").style.display="none";
+        document.getElementById("sm-player").style.display="block";
+        return;
+    }
+    else{
+        document.getElementById("sm-rules").style.display="block";
+        document.getElementById("sm-player").style.display="none";
+    }
 }
